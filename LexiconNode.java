8bed8a93e6f88bf328230de64103aac28e589641
Daniel W Barowy <dbarowy@cs.williams.edu>
2019-04-15 14:16:59 -0400

    Fix indentation for IntelliJ.

--- LexiconNode.java
+++ LexiconNode.java
@@ -2,9 +2,9 @@ import structure5.*;
 import java.util.Iterator;
 
 /*
-* TODO: implement and document this class
-* You may add helper methods and member variables as you see fit.
-*/
+ * TODO: implement and document this class
+ * You may add helper methods and member variables as you see fit.
+ */
 class LexiconNode implements Comparable<LexiconNode> {
 
     /* single letter stored in this node */
@@ -17,37 +17,37 @@ class LexiconNode implements Comparable<LexiconNode> {
     this LexiconNode */
 
     /**
-    * TODO: Constructor
-    */
+     * TODO: Constructor
+     */
     LexiconNode(char letter, boolean isWord) { }
 
     /**
-    * TODO: Compare this LexiconNode to another.
-    * (You should just compare the characters stored at the Lexicon
-    * Nodes.)
-    */
+     * TODO: Compare this LexiconNode to another.
+     * (You should just compare the characters stored at the Lexicon
+     * Nodes.)
+     */
     public int compareTo(LexiconNode o) { return 0; }
 
     /**
-    * TODO: Add LexiconNode child to correct position in child data
-    * structure
-    */
+     * TODO: Add LexiconNode child to correct position in child data
+     * structure
+     */
     public void addChild(LexiconNode ln) { }
 
     /**
-    * TODO: Get LexiconNode child for 'ch' out of child data
-    * structure
-    */
+     * TODO: Get LexiconNode child for 'ch' out of child data
+     * structure
+     */
     public LexiconNode getChild(char ch) { return null; }
 
     /**
-    * TODO: Remove LexiconNode child for 'ch' from child data structure
-    */
+     * TODO: Remove LexiconNode child for 'ch' from child data structure
+     */
     public void removeChild(char ch) { }
 
     /**
-    * TODO: create an Iterator that iterates over children in
-    * alphabetical order
-    */
+     * TODO: create an Iterator that iterates over children in
+     * alphabetical order
+     */
     public Iterator<LexiconNode> iterator() { return null; }
 }
